4509af482601b4bf964818f799852454d7ab960a
Nakarin010 <75832827+Nakarin010@users.noreply.github.com>
2023-04-20 14:21:11 +0700

    Create inSer.java

---
+++ inSer.java
@@ -0,0 +1,15 @@
+		//insertion sort technique
+//		int n = a.size();
+//		for (int i =1; i < n; i++) {
+//			String key = a.get(i);
+//			int j = i - 1;
+//			while (j >= 0 && a.get(j).compareTo(key) < 0) {
+//				a.set(j+1, a.get(j));
+//				j = j - 1;
+//			}
+//
+				a.set(j + 1, key);
+//				System.out.println("Pass "+(i)+": "+a);
+//
+//		}
+//
